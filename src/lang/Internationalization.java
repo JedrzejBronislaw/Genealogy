4e615c258e58e07aabf77a0bafa7f02c9aab6f39
JedrzejBronislaw <jedkru@wp.pl>
2020-04-02 10:06:05 +0200

    Add internationalization class

---
+++ src/lang/Internationalization.java
@@ -0,0 +1,32 @@
+package lang;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import lombok.Getter;
+
+public class Internationalization {
+	static final String resourceLocation = "lang.labels";
+	private static ResourceBundle rb = ResourceBundle.getBundle(resourceLocation);
+	
+	
+	@Getter
+	private static Languages currentLanguage;
+	
+	private Internationalization() {};
+	
+	
+	public static String get(String key) {
+		return rb.getString(key);
+	}
+	
+	private static void refresh() {
+		rb = ResourceBundle.getBundle(resourceLocation);
+	}
+	
+	public static void setLanguage(Languages language) {
+		Locale.setDefault(language.getLocale());
+		refresh();
+		currentLanguage = language;
+	}
+}
