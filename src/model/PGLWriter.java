20bd3d7e466bce9a7d2693039689027663d4a301
JedrzejBronislaw <jedkru@wp.pl>
2020-04-21 23:30:59 +0200

    Add PGL file writer

---
+++ src/model/PGLWriter.java
@@ -0,0 +1,167 @@
+package model;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import model.Person.LifeStatus;
+import model.Person.Sex;
+
+@RequiredArgsConstructor
+public class PGLWriter {
+	
+	@NonNull
+	private File file;
+	private FileWriter writer;
+	
+	private Tree tree;
+	
+	public boolean save(Tree tree) {
+		try {
+			if (tree == null) return false;
+			
+			writer = new FileWriter(file);
+			
+			this.tree = tree;
+			String[] ids = tree.getIDs();
+			
+			writeMainSection(tree);
+			List.of(ids).forEach(id -> {writePerson(id, tree.getPerson(id));});
+			
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return true;
+	}
+
+	private void writeMainSection(Tree tree) throws IOException {
+		SimpleDateFormat format = new SimpleDateFormat("hh:mm:ss yyyy-MM-dd");
+
+		writer.write("[Main]");
+		
+		writeNewLine();
+		writer.write("ost_otw=" + format.format(tree.getLastOpen()));
+		writeNewLine();
+		writer.write("wersja=" + format.format(tree.getLastModification()));
+		writeNewLine();
+		writer.write("ile=" + tree.numberOfPersons());
+		writeNewLine();
+
+		String[] names = tree.getCommonSurnames();
+		for(int i=0; i<names.length; i++) {
+			writer.write("nazw" + (i+1) + "=" + names[i]);
+			writeNewLine();			
+		}
+	}
+
+	private boolean writePerson(String id, Person person) {
+		try {
+			writePersonId(id);
+			saveProperty("imie", person.getFirstName());
+			saveProperty("nazwisko", person.getLastName());
+			saveProperty("ps", person.getAlias());
+			
+			saveProperty("datur", person.getBirthDate());
+			saveProperty("miejur", person.getBirthPlace());
+			saveProperty("datsm", person.getDeathDate());
+			saveProperty("miejsm", person.getDeathPlace());
+			
+			saveProperty(person.getLifeStatus());
+			saveProperty(person.getSex());
+			saveProperty("parafia", person.getBaptismParish());
+			saveProperty("mpoch", person.getBurialPlace());
+			
+			saveProperty("kontakt", person.getContact());
+			saveProperty("uwagi", person.getComments());
+			
+			
+			saveProperty("ojciec", person.getFather());
+			saveProperty("matka", person.getMother());
+			
+			saveMarriages(person);	
+			saveChildren(person);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
+	}
+
+	private void writePersonId(String id) throws IOException {
+		writer.write("[" + id + "]");
+		writeNewLine();
+	}
+
+	private void saveProperty(String name, String value) throws IOException {
+		if (value == null || value.isEmpty()) return;
+		
+		writer.write(name + "=" + value);
+		writeNewLine();
+	}
+
+	private void saveProperty(String name, int value) throws IOException {
+		writer.write(name + "=" + value);
+		writeNewLine();
+	}
+
+	private void saveProperty(String name, MyDate value) throws IOException {
+		if (value == null) return;
+		
+		writer.write(name + "=" + value.toString());
+		writeNewLine();
+	}
+
+	private void saveProperty(String name, Person value) throws IOException {
+		if (value == null) return;
+		String id = tree.getID(value);
+		if (id == null) return;
+		
+		writer.write(name + "=" + id);
+		writeNewLine();
+	}
+
+	private void saveProperty(LifeStatus value) throws IOException {
+		if (value == null || value == LifeStatus.UNDEFINED) return;
+		
+		if (value == LifeStatus.NO)  writer.write("zyje=0");
+		if (value == LifeStatus.YES) writer.write("zyje=1");
+		writeNewLine();
+	}
+
+	private void saveProperty(Sex value) throws IOException {
+		if (value == null || value == Sex.UNDEFINED) return;
+		
+		if (value == Sex.WOMEN) writer.write("plec=0");
+		if (value == Sex.MAN)   writer.write("plec=1");
+		writeNewLine();
+	}
+
+	private void saveMarriages(Person person) throws IOException {
+		saveProperty("malzenstwa", person.numberOfMarriages());
+		
+		for(int i=0; i<person.numberOfMarriages(); i++) {
+			saveProperty("malzonek" + (i+1), tree.getID(person.getSpouse(i)));
+			saveProperty("malzdata" + (i+1), person.getMarriages(i).getDate());
+			saveProperty("malzmjsc" + (i+1), person.getMarriages(i).getPlace());
+		}
+	}
+
+	private void saveChildren(Person person) throws IOException {
+		saveProperty("dzieci", person.numberOfChildren());
+		
+		for(int i=0; i<person.numberOfChildren(); i++)
+			saveProperty("dziecko" + (i+1), tree.getID(person.getChild(i)));
+	}
+
+	private void writeNewLine() throws IOException {
+		writer.write("\n");
+	}
+}
---
+++ test/model/PGLWriterTest.java
@@ -0,0 +1,180 @@
+package model;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+
+import model.Person.LifeStatus;
+import model.Person.Sex;
+
+public class PGLWriterTest {
+
+	public Tree prepareTree() {
+		Tree tree = new Tree();
+		Person father = prepareFather();
+		Person mother = prepareMother();
+		Person child = prepareChild();
+		
+		father.addMarriages(mother);
+		father.addChild(child);
+		mother.addMarriages(father);
+		mother.addChild(child);
+		child.setFather(father);
+		child.setMother(mother);
+		
+		tree.addPerson("1", father);
+		tree.addPerson("2", mother);
+		tree.addPerson("3", child);
+		
+		tree.addCommonSurname("Kowalski");
+		tree.addCommonSurname("Nowak");
+		tree.addCommonSurname("Kowal");
+		
+		tree.setLastOpen(tryLoadDate("11:32:16 01.04.2020", new SimpleDateFormat("hh:mm:ss dd.MM.yyyy")));
+		tree.setLastModification(tryLoadDate("08:12:55 06.05.2019", new SimpleDateFormat("hh:mm:ss dd.MM.yyyy")));
+		
+		return tree;
+	}
+
+	private Person prepareFather() {
+		Person person = new Person();
+		
+		person.setFirstName("Adam");
+		person.setLastName("Kowalski");
+		person.setAlias("Daniel");
+		person.setLifeStatus(LifeStatus.NO);
+		person.setSex(Sex.MAN);
+		person.setBirthDate(new MyDate("1.2.1908"));
+		person.setDeathDate(new MyDate("3.4.2010"));
+		person.setBirthPlace("Poznan");
+		person.setDeathPlace("Krakow");
+		person.setContact("contact");
+		person.setComments("comments");
+		person.setBaptismParish("parish");
+		person.setBurialPlace("cemetery");
+		
+		return person;
+	}
+
+	private Person prepareMother() {
+		Person person = new Person();
+		
+		person.setFirstName("Ewa");
+		person.setLastName("Nowak");
+		person.setSex(Sex.WOMEN);
+		
+		return person;
+	}
+
+	private Person prepareChild() {
+		Person person = new Person();
+		
+		person.setFirstName("Robert");
+		person.setLastName("Kowalski");
+		person.setSex(Sex.MAN);
+		
+		return person;
+	}
+	
+	private Date tryLoadDate(String textDate, SimpleDateFormat format) {
+		Date date;
+		try {
+			date = format.parse(textDate);
+		} catch (ParseException e) {
+			date = null;
+		}
+		
+		return date;
+	}
+	
+	private Tree saveAndLoadTree(Tree tree) {
+		File file = null;
+		Tree treeFromFile = new Tree();
+		PGLWriter writer;
+		PGLFile pglFile = null;
+		
+		try {
+			file = File.createTempFile("pglWriterTest", ".pgl");
+			file.deleteOnExit();
+		} catch (IOException e) {
+			fail("Creation file error");
+		}
+		
+		
+		writer = new PGLWriter(file);
+		if (!writer.save(tree)) fail("Saving file error");
+		
+		
+		try {
+			pglFile = new PGLFile(file.getAbsolutePath());
+		} catch (FileNotFoundException e) {
+			fail("Opening file error");
+		}
+		
+		if (!pglFile.load(treeFromFile)) fail("Loading file error");
+		
+		return treeFromFile;
+	}
+
+	@Test
+	public void equalsTree() {
+		Tree tree = prepareTree();
+		Tree treeFromFile = saveAndLoadTree(tree);
+
+		assertEquals(tree, treeFromFile);
+	}
+
+	@Test
+	public void numberOfPersons() {
+		Tree tree = prepareTree();
+		Tree treeFromFile = saveAndLoadTree(tree);
+		
+		assertEquals(tree.numberOfPersons(), treeFromFile.numberOfPersons());
+	}
+
+	@Test
+	public void commonSurnames() {
+		Tree tree = prepareTree();
+		Tree treeFromFile = saveAndLoadTree(tree);
+		
+		assertArrayEquals(tree.getCommonSurnames(), treeFromFile.getCommonSurnames());
+	}
+
+	@Test
+	public void lastOpenDate() {
+		Tree tree = prepareTree();
+		Tree treeFromFile = saveAndLoadTree(tree);
+		
+		assertEquals(tree.getLastOpen(), treeFromFile.getLastOpen());
+	}
+
+	@Test
+	public void lastModificationDate() {
+		Tree tree = prepareTree();
+		Tree treeFromFile = saveAndLoadTree(tree);
+		
+		assertEquals(tree.getLastModification(), treeFromFile.getLastModification());
+	}
+
+	@Test
+	public void equalsEachPerson() {
+		Tree tree = prepareTree();
+		Tree treeFromFile = saveAndLoadTree(tree);
+
+		String[] ids = tree.getIDs();
+		
+		List.of(ids).forEach(id ->
+			assertEquals("id="+id, tree.getPerson(id), treeFromFile.getPerson(id))
+		);
+	}
+}
