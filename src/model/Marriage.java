8146846dad1e562b5e119f60eb7f398df50fb514
JedrzejBronislaw <jedkru@wp.pl>
2020-05-13 12:49:00 +0200

    Add constructors to Marriage class

--- src/model/Marriage.java
+++ src/model/Marriage.java
@@ -2,11 +2,33 @@ package model;
 
 import lombok.Getter;
 import lombok.Setter;
+import model.tools.ManWoman;
 
 public class Marriage {
 			
 	@Setter @Getter private String date;
 	@Setter @Getter private String place;
 	@Setter @Getter private Person husband, wife;
+	
+	public Marriage() {}
+	
+	public Marriage(Person spouse1, Person spouse2) {
+		setSpouses(spouse1, spouse2);
+	}
+
+	public Marriage(Person spouse1, Person spouse2, String date, String place) {
+		setSpouses(spouse1, spouse2);
+		this.date = date;
+		this.place = place;
+	}
+	
+	private void setSpouses(Person spouse1, Person spouse2) {
+		ManWoman manWoman = new ManWoman(spouse1, spouse2);
+		
+		if (manWoman.success()) {
+			husband = manWoman.getMan();
+			wife = manWoman.getWoman();
+		}
+	}
 	
 }
