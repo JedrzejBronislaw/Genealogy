b86d04d355a1d3eefa51777c6222d57eb6c81352
JedrzejBronislaw <jedkru@wp.pl>
2020-05-19 16:32:17 +0200

    Improve saving tree graph (fx version) to file

--- src/treeGraphs/painter/fxPainter/FxGraphSaver.java
+++ src/treeGraphs/painter/fxPainter/FxGraphSaver.java
@@ -7,9 +7,11 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 import javafx.embed.swing.SwingFXUtils;
+import javafx.geometry.Bounds;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.image.WritableImage;
 import javafx.scene.layout.Pane;
+import javafx.scene.transform.Transform;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.Setter;
@@ -24,7 +26,7 @@ public class FxGraphSaver extends GraphSaver {
 	
 	@Override
 	public boolean save(File file) {
-		WritableImage writableImage = pane.snapshot(new SnapshotParameters(), null);
+		WritableImage writableImage = createScaledSnapshot(2);
 		RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
 
 		try {
@@ -36,4 +38,19 @@ public class FxGraphSaver extends GraphSaver {
 		
 		return true;
 	}
+	
+    private WritableImage createScaledSnapshot(int scale) {
+        final Bounds bounds = pane.getLayoutBounds();
+
+        final WritableImage image = new WritableImage(
+            (int) Math.round(bounds.getWidth() * scale),
+            (int) Math.round(bounds.getHeight() * scale));
+
+        final SnapshotParameters spa = new SnapshotParameters();
+        spa.setTransform(Transform.scale(scale, scale));
+
+        pane.snapshot(spa, image);
+        
+        return image;
+    }
 }
