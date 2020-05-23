d9f6cc3d1ab21b4bda492578170f102ad845e3fd
JedrzejBronislaw <jedkru@wp.pl>
2020-05-23 22:15:51 +0200

    Add onChange event to EditEnumField

--- src/viewFX/editPerson/fields/enumField/EditEnumFieldController.java
+++ src/viewFX/editPerson/fields/enumField/EditEnumFieldController.java
@@ -4,6 +4,7 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,12 +12,17 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
 import lang.Internationalization;
+import lombok.Setter;
+import tools.Injection;
 import viewFX.editPerson.fields.EditFieldInterface;
 
 public class EditEnumFieldController implements EditFieldInterface, Initializable {
 
 	@FXML
 	private VBox box;
+	
+	@Setter
+	private Consumer<String> onChange;
 	
 	private String value = "";
 	
@@ -34,7 +40,10 @@ public class EditEnumFieldController implements EditFieldInterface, Initializabl
 		RadioButton radio = new RadioButton(name);
 		
 		radio.setToggleGroup(radioGroup);
-		radio.setOnAction(e -> this.value = value);
+		radio.setOnAction(e -> {
+			this.value = value;
+			Injection.run(onChange, value);
+		});
 		return radio;
 	}
 	
