3605192e71aa7647f871605db76523b3dafd64d0
JedrzejBronislaw <jedkru@wp.pl>
2022-01-11 21:22:11 +0100

    Format PGLIncorrectLine

--- src/model/pgl/loader/PGLIncorrectLine.java
+++ src/model/pgl/loader/PGLIncorrectLine.java
@@ -10,23 +10,23 @@ public class PGLIncorrectLine {
 	private String content;
 	private Optional<String> sectionName;
 	
+	
+	public PGLIncorrectLine(String content) {
+		this(content, null);
+	}
+	
 	public PGLIncorrectLine(String content, String sectionName) {
 		validateContent(content);
 		
 		this.content = content;
 		this.sectionName = Optional.ofNullable(sectionName);
 	}
-	
-	public PGLIncorrectLine(String content) {
-		validateContent(content);
-		
-		this.content = content;
-		this.sectionName = Optional.empty();
-	}
 
 	
 	private void validateContent(String content) {
-		if (content == null) throw new IllegalArgumentException("Incorrect PGL line cannot be null.");
+		if (content == null)
+			throw new IllegalArgumentException("Incorrect PGL line cannot be null.");
+		
 		if (content.contains("="))
 			throw new ContentIsNotIncorrectException(content);
 	}
