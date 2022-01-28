8ae6655abed100d7acaddf1f4a16dc04e0834649
JedrzejBronislaw <jedkru@wp.pl>
2022-01-28 18:27:39 +0100

    Add tests for replacePolishChars()

---
+++ test/utils/UtilsTest.java
@@ -0,0 +1,56 @@
+package utils;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class UtilsTest {
+
+	@Test
+	public void shouldReplaceLowerPolishCharacters() {
+		// given
+		String polishChars = "πÊÍ≥ÒÛúøü";
+		
+		// when
+		String withoutPolishChars = Utils.replacePolishChars(polishChars);
+		
+		// then
+		assertEquals("acelnoszz", withoutPolishChars);
+	}
+
+	@Test
+	public void shouldReplaceUpperPolishCharacters() {
+		// given
+		String polishChars = "•∆ £—”åØè";
+		
+		// when
+		String withoutPolishChars = Utils.replacePolishChars(polishChars);
+		
+		// then
+		assertEquals("ACELNOSZZ", withoutPolishChars);
+	}
+
+	@Test
+	public void shouldReplaceMixedLowerAndUpperPolishCharacters() {
+		// given
+		String polishChars = "•∆ ≥ÒÛåØè";
+		
+		// when
+		String withoutPolishChars = Utils.replacePolishChars(polishChars);
+		
+		// then
+		assertEquals("ACElnoSZZ", withoutPolishChars);
+	}
+
+	@Test
+	public void shouldReplaceMixedPolishCharsWithOtherChar() {
+		// given
+		String polishChars = "x•x∆x x≥xÒxÛxåxØxèx";
+		
+		// when
+		String withoutPolishChars = Utils.replacePolishChars(polishChars);
+		
+		// then
+		assertEquals("xAxCxExlxnxoxSxZxZx", withoutPolishChars);
+	}
+}
